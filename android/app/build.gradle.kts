a60acc6b18e740d3ca272e2440d26a99f8842936
David <davidlombe62@gmail.com>
2025-09-28 10:35:29 +0200

    added camera feature but the ndkversion needs to be updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,16 @@ plugins {
 
 android {
     namespace = "com.example.tomotoe_disease_detection_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.example.tomotoe_disease_detection_app"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -19,21 +27,10 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tomotoe_disease_detection_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Using debug signing so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
